17bc776646aa802f4cc9d6a0b0fa2e1e1b580375
Prince179 <103314360+Prince179@users.noreply.github.com>
2024-03-18 11:35:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.post('/bfhl', (req, res) => {
+  const { data} = req.body;
+  const name="prince";
+  const dob="10112003";
+  const email="prince1794.be21@chitkara.edu.in";
+  const roll_number="2110991794";
+  const userId = `${name}_${dob}`;
+  const is_success = true;
+  const evenNumbers = [];
+  const oddNumbers = [];
+  const alphabets = [];
+  for (let num of data) {
+    if (typeof num === 'number') {
+      if (num % 2 === 0) {
+        evenNumbers.push(num);
+      } else {
+        oddNumbers.push(num);
+      }
+    } else if (typeof num === 'string') {
+      alphabets.push(num.toUpperCase());
+    }
+  }
+  const response = {
+    user_id: userId,
+    is_success,
+    email,
+    roll_number,
+    even_numbers: evenNumbers,
+    odd_numbers: oddNumbers,
+    alphabets
+  };
+  res.status(200).json(response);
+});
+app.get('/',(req,res)=>{
+  res.send("<h1>Hello World</h1>");
+})
+app.listen(3000,()=>{
+  console.log("server is running on port 3000");
+});
